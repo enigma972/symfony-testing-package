b75186142595512d7b8abc26ba404e4584e3eb25
Joel Lusavuvu <lusavuvujoel39@gmail.com>
2020-09-08 08:26:19 +0100

    setup enigma972 user bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Enigma972\UserBundle\Enigma972UserBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['all' => true],
 ];
